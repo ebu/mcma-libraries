fddef4878b36a05c38a733fab7623c36b61fcc5c
evanverneyfink <evan@triskel-inc.com>
2019-06-06 09:18:56 -0400

    Update libraries to version 0.3.0

--- nodejs/publish-all-npm.bat
+++ nodejs/publish-all-npm.bat
@@ -1,15 +1,15 @@
 cd mcma-core
-call npm version %1
+call npm version %1 --allow-same-version
 call npm publish
 cd ../mcma-data
-call npm version %1
+call npm version %1 --allow-same-version
 call npm publish
 cd ../mcma-api
-call npm version %1
+call npm version %1 --allow-same-version
 call npm publish
 cd ../mcma-worker
-call npm version %1
+call npm version %1 --allow-same-version
 call npm publish
 cd ../mcma-aws
-call npm version %1
+call npm version %1 --allow-same-version
 call npm publish
