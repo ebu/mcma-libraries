771a03307b374e84b5be483623e683e5b6761d21
evanverneyfink <evan@triskel-inc.com>
2019-12-11 11:11:29 -0500

    * Missed updating IResourceManagerProvider

--- dotnet/base/Mcma.Client/IResourceManagerProvider.cs
+++ dotnet/base/Mcma.Client/IResourceManagerProvider.cs
@@ -1,7 +1,11 @@
+using System.Net.Http;
+
 namespace Mcma.Client
 {
     public interface IResourceManagerProvider
     {
+        ResourceManager Get(HttpClient httpClient, ResourceManagerConfig config = null);
+
         ResourceManager Get(ResourceManagerConfig config = null);
     }
 } 
