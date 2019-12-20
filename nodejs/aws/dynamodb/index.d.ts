57c8fc8e21b4fdcf3ea543164168206bf667de19
Joost Rovers <joost@rovers.pt>
2019-12-20 13:35:03 +0000

    Fixed Typescript definition of DynamoDbTable

--- nodejs/aws/dynamodb/index.d.ts
+++ nodejs/aws/dynamodb/index.d.ts
@@ -2,7 +2,7 @@ import { Resource, ResourceType } from "@mcma/core";
 import { DbTable, DbTableProvider } from "@mcma/data";
 
 export class DynamoDbTable<T extends Resource> extends DbTable<T> {
-    constructor(type: string, tableName: string);
+    constructor(tableName: string, type: ResourceType<T>);
 }
 
 export class DynamoDbTableProvider<T extends Resource> implements DbTableProvider<T> {
