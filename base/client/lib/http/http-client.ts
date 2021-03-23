24ec4f267bb4c73e2cf5f6b9630aa720ff5a96a4
Joost Rovers <joost@rovers.pt>
2021-03-23 12:25:18 +0000

    Http client now signs request before each attempt, so it avoids a signature timeout

--- base/client/lib/http/http-client.ts
+++ base/client/lib/http/http-client.ts
@@ -104,19 +104,24 @@ export class HttpClient implements Http {
             throw new McmaException("HttpClient: Missing url in request config");
         }
 
-        if (this.authenticator) {
-            await this.authenticator.sign(config);
-        }
-
         // add tracker header, if a tracker is present
         if (config.tracker) {
             config.headers[McmaHeaders.tracker] = Utils.toBase64(JSON.stringify(config.tracker));
             delete config.tracker;
         }
 
+        // storing original headers so we can safely run the authentication signer multiple times
+        const headers = JSON.parse(JSON.stringify(config.headers));
+
         // send request using axios
         for (let attempts = 0; attempts < this.config.maxAttempts; attempts++) {
             try {
+                config.headers = headers;
+
+                if (this.authenticator) {
+                    await this.authenticator.sign(config);
+                }
+
                 return await axios(config);
             } catch (error) {
                 if (attempts < this.config.maxAttempts - 1) {
