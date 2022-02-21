b283f8831c30d666d32f01e41be21c6fc66cfc38
Joost Rovers <joost@rovers.pt>
2022-02-21 19:29:14 +0000

    Removed import "os" as it is not browser compatible.

--- base/core/lib/mcma-exception.ts
+++ base/core/lib/mcma-exception.ts
@@ -1,32 +1,35 @@
-import { EOL } from "os";
-
 export class McmaException extends Error {
-    constructor(public message: string, public cause?: McmaException, public context?: any) {
+    public originalStack: string;
+
+    constructor(public message: string, public cause?: Error, public context?: any) {
         super(message);
         this.cause = cause;
         this.context = context;
 
+        this.originalStack = this.stack;
         this.stack = this.toString(true);
     }
 
     toString(includeDetails = false): string {
         let ret = "";
 
-        let c: McmaException = this;
+        let c: any = this;
         while (c) {
-            if (includeDetails && c.stack) {
+            if (includeDetails && c.originalStack) {
+                ret += c.originalStack;
+            } else if (includeDetails && c.stack) {
                 ret += c.stack;
             } else {
                 ret += "Error: " + (c.message ?? c);
             }
 
             if (includeDetails && c.context) {
-                ret += EOL + "Context:" + EOL + JSON.stringify(c.context, null, 2);
+                ret += "\nContext:\n" + JSON.stringify(c.context, null, 2);
             }
 
             c = c.cause;
             if (c) {
-                ret += EOL + "Caused by: " + EOL;
+                ret += "\nCaused by:\n";
             }
         }
 
