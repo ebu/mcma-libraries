2f3e73061e9a5bb07e0e807d26d292c88aedfb8c
Joost Rovers <joost@rovers.pt>
2023-08-18 14:55:37 +0100

    WIP v0.16.3 Improved implementation of QueueWorkerInvoker. Added secretsProvider to ProviderCollection

--- azure/queue-worker-invoker/lib/queue-worker-invoker.ts
+++ azure/queue-worker-invoker/lib/queue-worker-invoker.ts
@@ -1,18 +1,29 @@
-import { QueueServiceClient } from "@azure/storage-queue";
+import { DefaultAzureCredential, TokenCredential } from "@azure/identity";
+import { StorageSharedKeyCredential, AnonymousCredential, QueueClient } from "@azure/storage-queue";
 
 import { Utils } from "@mcma/core";
 import { WorkerInvoker, WorkerRequestProperties } from "@mcma/worker-invoker";
 
-const appStorageConnectionString = process.env.WEBSITE_CONTENTAZUREFILECONNECTIONSTRING;
+export interface QueueWorkerInvokerConfig {
+    credentials?: StorageSharedKeyCredential | AnonymousCredential | TokenCredential
+}
 
 export class QueueWorkerInvoker extends WorkerInvoker {
-    constructor() {
+    constructor(private readonly config?: QueueWorkerInvokerConfig) {
         super();
+
+        if (!this.config) {
+            this.config = {};
+        }
+        if (!this.config.credentials) {
+            if (!this.config.credentials) {
+                this.config.credentials = new DefaultAzureCredential();
+            }
+        }
     }
 
     protected async invokeWorker(workerFunctionId: string, workerRequest: WorkerRequestProperties): Promise<void> {
-        const queueStorageClient = QueueServiceClient.fromConnectionString(appStorageConnectionString);
-        const queueClient = queueStorageClient.getQueueClient(workerFunctionId);
+        const queueClient = new QueueClient(workerFunctionId, this.config.credentials);
         await queueClient.sendMessage(Utils.toBase64(JSON.stringify(workerRequest)));
     }
 }
--- base/worker/lib/provider-collection.ts
+++ base/worker/lib/provider-collection.ts
@@ -1,12 +1,14 @@
 import { LoggerProvider, McmaException } from "@mcma/core";
 import { AuthProvider, ResourceManagerProvider } from "@mcma/client";
 import { DocumentDatabaseTableProvider } from "@mcma/data";
+import { SecretsProvider } from "@mcma/secrets";
 
 export interface Providers {
     authProvider?: AuthProvider;
     dbTableProvider?: DocumentDatabaseTableProvider;
     loggerProvider?: LoggerProvider;
     resourceManagerProvider?: ResourceManagerProvider;
+    secretsProvider?: SecretsProvider;
 }
 
 export class ProviderCollection implements Providers {
@@ -14,12 +16,14 @@ export class ProviderCollection implements Providers {
     private readonly _dbTableProvider: DocumentDatabaseTableProvider;
     private readonly _loggerProvider: LoggerProvider;
     private readonly _resourceManagerProvider: ResourceManagerProvider;
+    private readonly _secretsProvider: SecretsProvider;
 
     constructor(providers: Providers) {
         this._authProvider = providers?.authProvider;
         this._dbTableProvider = providers?.dbTableProvider;
         this._loggerProvider = providers?.loggerProvider;
         this._resourceManagerProvider = providers?.resourceManagerProvider;
+        this._secretsProvider = providers?.secretsProvider;
     }
 
     get authProvider() {
@@ -48,5 +52,12 @@ export class ProviderCollection implements Providers {
             throw new McmaException("ResourceManagerProvider not available");
         }
         return this._resourceManagerProvider;
+    }
+
+    get secretsProvider() {
+        if (!this._secretsProvider) {
+            throw new McmaException("SecretsProvider not available");
+        }
+        return this._secretsProvider;
     }
 }
