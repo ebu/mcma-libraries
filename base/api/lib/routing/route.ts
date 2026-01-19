2cafbceef8ed75c2f8d1bd2725c786ba95fc719b
Joost Rovers <joost@rovers.pt>
2026-01-19 14:57:48 +0000

    Enabled esModuleInterop to make project compile again

--- base/api/lib/routing/route.ts
+++ base/api/lib/routing/route.ts
@@ -1,9 +1,10 @@
-import * as uriTemplates from "uri-templates";
+import { default as uriTemplates } from "uri-templates";
 import { McmaApiRouteHandler } from "./route-handler";
 
 declare module "uri-templates" {
     interface URITemplate {
         test(url: string, opts?: { [key: string]: any }): boolean;
+
         fromUri(url: string, opts?: { [key: string]: any }): { [key: string]: string };
     }
 }
